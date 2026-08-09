7c4e03a65a5fccc55be3e08463a70fb87bfb6781
Alucard <fire0lord0zuko@proton.me>
2026-08-09 18:17:53 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define BUFFER_SZ 2048
 char BUFFER[BUFFER_SZ];
@@ -10,13 +11,13 @@ int main(int argc, char *argv[])
     // Flush after every printf
     setbuf(stdout, NULL);
 
-    // TODO: Uncomment the code below to pass the first stage
-    printf("$ ");
-
-    if (fgets(BUFFER, BUFFER_SZ, stdin) != NULL) {
-        size_t len = strlen(BUFFER);
-        printf("%.*s: command not found", (int)len-1, BUFFER);
-        fflush(stdout);
+    while (true) {
+        printf("$ ");
+        if (fgets(BUFFER, BUFFER_SZ, stdin) != NULL) {
+            size_t len = strlen(BUFFER);
+            printf("%.*s: command not found\n", (int)len-1, BUFFER);
+            fflush(stdout);
+        }
     }
 
 
