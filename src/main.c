e164ad37342be9912075215540379639774ebdac
Alucard <fire0lord0zuko@proton.me>
2026-08-09 18:06:08 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFER_SZ 2048
+char BUFFER[BUFFER_SZ];
+
 int main(int argc, char *argv[])
 {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+    // TODO: Uncomment the code below to pass the first stage
+    printf("$ ");
 
-  return 0;
+    if (fgets(BUFFER, BUFFER_SZ, stdin) != NULL) {
+        printf("{%s}: command not found", BUFFER);
+        fflush(stdout);
+    }
+
+
+    return 0;
 }
