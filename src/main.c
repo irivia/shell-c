03934f964d05d4f1fd574259be650de4165ab35c
Alucard <fire0lord0zuko@proton.me>
2026-08-15 03:21:23 +0300

    Add null check

--- src/main.c
+++ src/main.c
@@ -72,6 +72,8 @@ String trim_left_by_delim(const String s, char delim)
 
 StrList split_by_delim(char *s, char delim)
 {
+    if (s == NULL) return (StrList){0};
+
     StrList words = {0};
     size_t cur_len = 0;
     char *cur = s;
