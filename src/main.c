ef03b506c453453d59e50ec307f6a1b6f07bd1e8
Alucard <fire0lord0zuko@proton.me>
2026-08-18 22:25:57 +0300

    Break instead of return in cmd_name_completion()

--- src/main.c
+++ src/main.c
@@ -739,8 +739,7 @@ char** cmd_name_completion(const char *text, int start, int end)
             return arr;
         }
         else {
-            da_free(words);
-            return NULL;
+            break;
         }
     }
 
