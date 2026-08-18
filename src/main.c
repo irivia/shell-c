cd72d954062616b470b9a7d141792e52c1eecde2
Alucard <fire0lord0zuko@proton.me>
2026-08-18 04:53:08 +0300

    Enable filename completions after command completion

--- src/main.c
+++ src/main.c
@@ -611,7 +611,7 @@ char* cmd_name_generator(const char *text, int state)
 
 char ** cmd_name_completion(const char *text, int start, int end)
 {
-    rl_attempted_completion_over = 1;
+    rl_attempted_completion_over = 0;
     return rl_completion_matches(text, cmd_name_generator);
 }
 
